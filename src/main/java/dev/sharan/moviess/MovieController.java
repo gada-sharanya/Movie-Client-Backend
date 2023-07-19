f76161e2fdf980921d99d39740e085bcb7601e6e
Sharanya Gada <gadasharanya@gmail.com>
2023-07-19 14:04:51 -0400

    Initally commit

---
+++ src/main/java/dev/sharan/moviess/MovieController.java
@@ -0,0 +1,30 @@
+package dev.sharan.moviess;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getallMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId),HttpStatus.OK);
+    }
+}
---
+++ src/test/java/dev/sharan/moviess/MoviessApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sharan.moviess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
